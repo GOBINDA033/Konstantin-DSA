bed2d976f342f7fc086ec355f00b05f86b58f9d4
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2025-11-05 22:43:12 +0530

    longest subarray with sum k
    This problem is solved by using brute force approach.

---
+++ LongestSubarrayBruteForce.java
@@ -0,0 +1,32 @@
+public class LongestSubarrayBruteForce {
+    public static int BruteForceApproach(int []arr,int k)
+    {
+        int maxlen=0;
+        int n=arr.length;
+for(int i=0;i<n;i++)//start index
+{
+for(int j=i;j<n;j++)//end index
+{
+int sum=0;
+for(int p=i;p<=j;p++)
+{
+    sum+=arr[p];
+
+}
+
+if(sum==k)
+{
+maxlen=Math.max(maxlen, j-i+1);
+}
+}
+}
+return maxlen;
+}
+
+    public static void main(String[] args) {
+        int arr[]={0,0,1,2,1,1,1,1,0,2,1,2,4,1,0,};
+        int k=6;
+        int result=BruteForceApproach(arr,k);
+        System.out.println(result);
+    }
+}
