2c56772366886504cd03cbd068aefe07ce3c38ab
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2025-12-15 00:24:16 +0530

    Add files via upload

---
+++ RearrangementArrayElement.java
@@ -0,0 +1,30 @@
+public class RearrangementArrayElement {
+   public static int[] rearrangementArrayElement(int[] nums)
+   {
+    int [] ans =new int[nums.length];
+    int pos=0;
+    int neg=1;
+    for(int i=0;i<nums.length;i++)
+    {
+        if(nums[i]>0)
+        {
+ans[pos]=nums[i];
+pos=pos+2;
+        }
+        else{
+            ans[neg]=nums[i];
+            neg=neg+2;
+        }
+    }
+    return ans;
+
+   }
+   public static void main(String[] args) {
+    int []nums={3,-1,4,1,-1};
+int []result=rearrangementArrayElement(nums);
+for(int i=0;i<nums.length;i++){
+    System.out.println(result[i]);
+}
+   }
+
+}
